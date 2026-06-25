88afe205448da31b863b7d626e2f11340ff2d847
harsha balakrishnan <harsha16balakrishnan@proton.me>
2026-06-25 22:35:06 +0530

    style: implement v2.1 Poetic Theatre design tokens in sanctuary.css

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,5 +1,6 @@
 import { render } from 'preact';
 import { App } from './App';
+import '../styles/sanctuary.css';
 import './styles/global.css';
 import './styles/layout.css';
 import './styles/library.css';
